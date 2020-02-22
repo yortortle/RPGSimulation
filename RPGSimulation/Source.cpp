588cc6600815cc7e0add32ad8fba55c152295bf7
Troy <tnoblehancock@gmail.com>
2020-02-22 12:39:06 -0500

    grunt work

--- RPGSimulation/Source.cpp
+++ RPGSimulation/Source.cpp
@@ -8,11 +8,18 @@
 
 using namespace std;
 
-// a couple rules:
+//simulating damage curve in games with class selection, class attributes, and damage modifiers for specific spells that may or may not synergize with those classes.
+//You will be able to make a character object which corresponds to class selection
+// you will be able to declare a spell which takes in that class object as it's argument, and determine it's damage output
+//you will be able to interact using an interaction object which will take two spell objects and print which spell has the higher damage output and maximized efficiency in terms of the 
+//damage formula.
+
+
+// a couple rules for myself:
 //make parent class of Spell and People have a protected constructor so it can't be called directly (abstract class in some sense? Or is that only if it has a pure virtual function)
 //make functions const that I don't plan on changing variables within, this prevents issues down the line in the code.
 //pass in by reference more into functions - try to understand when and where copying the variable versus not is better for efficiency. I will probably 
-//not fully understand as i'm doing this, that's okay, it's to practice and understand.
+//not fully understand as i'm doing this, that's okay, it's to practice and understand; upon reflection it will make more sense.
 
 //goals:
 //implement virtual functions
@@ -31,9 +38,11 @@ using namespace std;
 
 int main()
 {
-	Bladee drain("Jeff", 1000);
-	IceBarrage spell(drain);
-	spell.getDamage();
-	//spell.calculateDmg();
-	cout << spell.getDamage() << endl;
+	Bladee jeff("Jeff", 1000);
+	IceBarrage bladeeBarrage(jeff);
+	cout << bladeeBarrage.getDamage() << endl;
+	
+	Drain troy("troy", 1000);
+	IceBarrage troyBarrage(troy);
+	cout << troyBarrage.getDamage() << endl;
 }
--- RPGSimulation/Spells.h
+++ RPGSimulation/Spells.h
@@ -6,6 +6,8 @@ class Spell
 protected: 
 	float dmg = 0;
 
+	//I don't necessarily get an error when I don't intialize these member variables immediately, and I don't see a reason to. Why does it warn me here when I plan on intializing them later on?
+
 	float AgiModifier;
 	float FireModifier;
 	float ChaosModifier;
@@ -35,6 +37,22 @@ public:
 	{
 		return "say";
 	}
+
+	float calculateDmg(Person& person)
+	{
+		dmg =
+			(AgiModifier * person.Agility) +
+			(FireModifier * person.Firebending) +
+			(ChaosModifier * person.Chaos) +
+			(DarkModifier * person.Darkness) +
+			(FrostModifier * person.FrostWork) +
+			(SpellModifier * person.SpellCraft) +
+			(RuneModifier * person.RuneCraft) +
+			(MomentumModifier * person.Momentum);
+
+		return dmg;
+	}
+
 };
 
 class IceBarrage : public Spell
@@ -58,30 +76,153 @@ public:
 		RuneModifier = 1.0f;
 		MomentumModifier = 1.7f;
 
-		cout << calculateDmg(person) << endl;
+		calculateDmg(person);
 	}
 	~IceBarrage() override
 	{
-		cout << "ice has been destroyed" << endl;
-	}
-
-	void spellsay()
-	{
-		cout << "spellsay" << endl;
-	}
-
-	float calculateDmg(Person& person)
-	{
-		dmg =
-			(AgiModifier * person.Agility) +
-			(FireModifier * person.Firebending) +
-			(ChaosModifier * person.Chaos) +
-			(DarkModifier * person.Darkness) +
-			(FrostModifier * person.FrostWork) +
-			(SpellModifier * person.SpellCraft) +
-			(RuneModifier * person.RuneCraft) +
-			(MomentumModifier * person.Momentum);
-
-		return dmg;
+		cout << "ice barrage has been destroyed" << endl;
 	}
 };
+
+class IceBlitz : public Spell
+{
+
+public:
+	IceBlitz(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 1.2f;
+		FireModifier = .8f;
+		ChaosModifier = 1.4f;
+		DarkModifier = 1.1f;
+		FrostModifier = 1.5f;
+		SpellModifier = 1.3f;
+		RuneModifier = 1.3f;
+		MomentumModifier = 2.0f;
+
+		calculateDmg(person);
+	}
+};
+
+class FireWave : public Spell
+{
+
+public:
+	FireWave(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 0.8f;
+		FireModifier = 2.0f;
+		ChaosModifier = 1.2f;
+		DarkModifier = 1.2f;
+		FrostModifier = 0.5f;
+		SpellModifier = 1.3f;
+		RuneModifier = 1.6f;
+		MomentumModifier = 1.2f;
+
+		calculateDmg(person);
+	}
+};
+
+class FireDance : public Spell
+{
+
+public:
+	FireDance(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 2.0f;
+		FireModifier = 1.5f;
+		ChaosModifier = 0.8f;
+		DarkModifier = 1.0f;
+		FrostModifier = 0.5f;
+		SpellModifier = 2.0f;
+		RuneModifier = 1.2f;
+		MomentumModifier = 2.0f;
+
+		calculateDmg(person);
+	}
+};
+
+class DarkDash : public Spell
+{
+
+public:
+	DarkDash(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 2.0f;
+		FireModifier = 0.5f;
+		ChaosModifier = 1.6f;
+		DarkModifier = 2.0f;
+		FrostModifier = 0.5f;
+		SpellModifier = 1.3f;
+		RuneModifier = 1.6f;
+		MomentumModifier = 2.0f;
+
+		calculateDmg(person);
+	}
+};
+
+class DarkPrism : public Spell
+{
+
+public:
+	DarkPrism(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 0.5f;
+		FireModifier = 1.5f;
+		ChaosModifier = 1.0f;
+		DarkModifier = 2.0f;
+		FrostModifier = 0.7f;
+		SpellModifier = 1.6f;
+		RuneModifier = 1.2f;
+		MomentumModifier = 0.9f;
+
+		calculateDmg(person);
+	}
+};
+
+class WindChant : public Spell
+{
+
+public:
+	WindChant(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 2.0f;
+		FireModifier = 1.2f;
+		ChaosModifier = 0.5f;
+		DarkModifier = 1.0f;
+		FrostModifier = 1.2f;
+		SpellModifier = 1.1f;
+		RuneModifier = 0.5f;
+		MomentumModifier = 2.0f;
+
+		calculateDmg(person);
+	}
+};
+
+class WindSlide : public Spell
+{
+
+public:
+	WindSlide(Person& person)
+		: Spell{ person }
+	{
+		AgiModifier = 1.2f;
+		FireModifier = 1.8f;
+		ChaosModifier = 1.0f;
+		DarkModifier = 0.5f;
+		FrostModifier = 1.2f;
+		SpellModifier = 2.0f;
+		RuneModifier = 2.0f;
+		MomentumModifier = 0.7f;
+
+		calculateDmg(person);
+	}
+};
+
+
+
